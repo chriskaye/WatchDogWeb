65927f90e0c45c6e9e0e555316cf7a952ae2c58c
Chris Kaye <70633994+chriskaye@users.noreply.github.com>
2026-07-19 12:17:23 +0100

    update .gitignore

--- db/stop.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Backing up database...
-docker exec watchdogweb-db-1 pg_dump -U postgres -d postgres -F c -b -f /backups/db.dump
-echo Stopping containers...
-docker compose down
-
-echo Done.
-pause
